6fc54baf88e837f78d3c2a0f472ca054858fc36a
Ari <ariosym@gmail.com>
2023-12-31 20:25:47 -0500

    cleaned up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import FilePlayerPage from './FilePlayerPage'
-import CanvasPage from '@/_components/CanvasPage'
-import QueryPlayer from '@/_components/QueryPlayer'
-import TimelinePage from '@/Timeline/Timeline'
-// import TimelineEditorTest from '@/TimelineEditorTest'
+import QueryPlayerPage from '@/_components/QueryPlayer'
 
 const DEFAULT_VIDEO_PATH = import.meta.env.VITE_DEFAULT_VIDEO_PATH;
 const DEFAULT_DIR_PATH = import.meta.env.VITE_DEFAULT_DIR_PATH;
@@ -15,19 +12,15 @@ export default function App() {
 
       <Routes>
 
-        <Route path="/" element={<FilePlayerPage 
-          // todo: REPLACE these props with a in-route call to react-query
-          defaultDirPath={DEFAULT_DIR_PATH}
-          defaultVideoPath={DEFAULT_VIDEO_PATH}
-        />} />
+        <Route path="/" element={
+          <FilePlayerPage 
+            // todo: REPLACE these props with a in-route call to react-query
+            defaultDirPath={DEFAULT_DIR_PATH}
+            defaultVideoPath={DEFAULT_VIDEO_PATH}
+          />
+        } />
         
-        <Route path="/canvas" element={<CanvasPage videoPath={DEFAULT_VIDEO_PATH} />}  />
-
-        <Route path="timeline" element={<TimelinePage videoPath={DEFAULT_VIDEO_PATH} />} />
-
-        <Route path="query-player" element={<QueryPlayer videoPath={DEFAULT_VIDEO_PATH} />} />
-
-        {/* <Route path="timeline-editor" element={<TimelineEditorTest />} /> */}
+        <Route path="query-player" element={<QueryPlayerPage videoPath={DEFAULT_VIDEO_PATH} />} />
 
       </Routes>
 
