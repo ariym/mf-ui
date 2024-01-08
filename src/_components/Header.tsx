38c36e7085c53bbd66dc708395a7334d7d8d5e54
Ari <ariosym@gmail.com>
2024-01-08 11:38:39 -0500

    renamed links

--- src/_components/Header.tsx
+++ src/_components/Header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
   return (
     <header className="py-5 px-5 max-w-screen-md mx-auto flex justify-between items-center">
       <span className="text-4xl"><a href="/">mf-catalog</a></span>
-      <span className="text-lg"><a href="ingest">ingest</a> | <a href="process">process</a> | <a href="queue">jobs</a></span>
+      <span className="text-lg"><a href="ingest">import</a> | <a href="process">process</a> | <a href="queue">queue</a></span>
     </header>
   )
 }
