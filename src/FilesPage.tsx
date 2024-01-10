e9f16922dfe1badb09f5e5892e88349fa16ac8f8
Ari <ariosym@gmail.com>
2024-01-10 16:42:46 -0500

    installed react-pro-sidebar

--- src/FilesPage.tsx
+++ src/FilesPage.tsx
@@ -13,39 +13,191 @@ import {
   CardTitle,
 } from "@/_components/ui/card"
 
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/_components/ui/drawer"
+
+import {
+  Menubar,
+  MenubarCheckboxItem,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarRadioGroup,
+  MenubarRadioItem,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from "@/_components/ui/menubar"
+
 import { Checkbox } from "@/_components/ui/checkbox"
 
-type IngestPage = {
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/_components/ui/tabs"
 
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+
+
+
+type IngestPage = {}
+
+
+
+export function TabsDemo() {
+  return (
+    <Tabs defaultValue="account" className="">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="browse-files">Files</TabsTrigger>
+        <TabsTrigger value="add-source">Sources</TabsTrigger>
+      </TabsList>
+      <TabsContent value="account">
+        <Card>
+          <CardHeader>
+            <CardTitle>Account</CardTitle>
+            <CardDescription>
+              Make changes to your account here. Click save when you're done.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" defaultValue="Pedro Duarte" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="username">Username</Label>
+              <Input id="username" defaultValue="@peduarte" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save changes</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value="password">
+        <Card>
+          <CardHeader>
+            <CardTitle>Password</CardTitle>
+            <CardDescription>
+              Change your password here. After saving, you'll be logged out.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="current">Current password</Label>
+              <Input id="current" type="password" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="new">New password</Label>
+              <Input id="new" type="password" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save password</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+    </Tabs>
+  )
 }
+
+
+
+const UniSidebar = () => (
+  <Sidebar>
+    <Menu>
+      <SubMenu label="Charts">
+        <MenuItem> Pie charts </MenuItem>
+        <MenuItem> Line charts </MenuItem>
+      </SubMenu>
+      <MenuItem> Documentation </MenuItem>
+      <MenuItem> Calendar </MenuItem>
+    </Menu>
+  </Sidebar>
+)
+
 
 export default function IngestPage({ }: IngestPage) {
 
   const [dirTree, udpateDirTree]: [any, any] = useState([]);
 
   return (
-    <div className="flex items-center flex-col">
+    <div style={{display: 'flex', height: '100%', minHeight: '400px'}}>
+    <UniSidebar />
+      <div className="flex items-center flex-col p-10">
 
-      <div className='flex flex-wrap justify-center'>
-        <DownloadCard
-          title="YouTube Downloader"
-          desc="saves video, transcript, and comments"
-          label="Video ID"
-          placeholder="OpyIY7ixgI8"
-        />
-        <DownloadCard
-          title="Twitter"
-          desc="only saves video"
-          label="Twitter Link"
-          placeholder="https://x.com/Rubber0Cement/status/1742024413917446225"
-        />
+        <Ingest />
+
+        <TabsDemo />
+
       </div>
-
     </div>
   )
 }
 
 
+
+export function Ingest() {
+  return (
+    <Drawer>
+      <DrawerTrigger>Ingest</DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Add Content</DrawerTitle>
+          <DrawerDescription>You can add a single file or an auto-sync Source.</DrawerDescription>
+        </DrawerHeader>
+
+        <div className='flex flex-wrap justify-center px-5'>
+          <DownloadCard
+            title="YouTube Downloader"
+            desc="saves video, transcript, and comments"
+            label="Video ID"
+            placeholder="OpyIY7ixgI8"
+          />
+
+          <DownloadCard
+            title="Twitter"
+            desc="only saves video"
+            label="Twitter Link"
+            placeholder="https://x.com/Rubber0Cement/status/1742024413917446225"
+          />
+        </div>
+
+        <DrawerFooter>
+          {/* <Button>Submit</Button> */}
+          <DrawerClose>
+            <Button variant="outline">Close</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+
+
+  )
+}
+
+
+
+type DownloadCard = {
+  title: string,
+  desc: string,
+  label: string,
+  placeholder: string
+  // api
+}
 export function DownloadCard({
   title,
   desc,
@@ -54,7 +206,7 @@ export function DownloadCard({
   // api
 }: DownloadCard) {
   return (
-    <Card className="w-[350px] m-2">
+    <Card className="w-[350px]">
       <CardHeader>
         <CardTitle>{title}</CardTitle>
         <CardDescription>{desc}</CardDescription>
@@ -75,12 +227,4 @@ export function DownloadCard({
       </CardFooter>
     </Card>
   )
-}
-
-type DownloadCard = {
-  title: string,
-  desc: string,
-  label: string,
-  placeholder: string
-  // api
 }
